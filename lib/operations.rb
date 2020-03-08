9ea6ab4220d7c593b5ab733eb6c86d7d7ad80b4c
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-08 02:30:14 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 ## INPUTS --> integer 
 ## OUTPUTS --> boolean 
 
-## below 40 or over 60 = unsafe 
-## else safe 
+## below 40 or over 60 = unsafe = true 
+## else safe = false 
 
 
 def unsafe?(speed)
+  if speed == speed > 60 || speed < 40
+    true
+  else 
+    false
+  end 
   
 end 
 
